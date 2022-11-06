4a583a16ea23fc7d8f20cc2c38dc0d7d4a070853
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-11-06 13:04:28 +0100

    chore: setup UI library

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+
+console.log(isDark.value)
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/register">Register</RouterLink>
-        <RouterLink to="/login">login</RouterLink>
-        <RouterLink to="/dashboard">dashboard</RouterLink>
-        <RouterLink to="/characters/1">character 1</RouterLink>
-        <RouterLink to="/leaderboard">leaderboard</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <html class="dark">
+    <el-button type="primary" @click="toggleDark()">toggle dark mode</el-button>
 
-  <RouterView />
+    <RouterView />
+  </html>
 </template>
