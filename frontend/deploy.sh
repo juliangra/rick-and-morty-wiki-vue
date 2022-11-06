16112301cd1d3a0d3a5f3fc9ae32c3e3a616fa69
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-11-06 15:41:13 +0100

    chore: use new mysql database for project4

---
+++ frontend/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+destination="/var/www/html/project4"
+
+echo "📦 Installing dependencies..."
+npm ci
+
+echo "🛠️ Building project..."
+npm run build:prod
+
+# If folder exists, remove it
+if [ -d $destination ]
+then
+    sudo rm -rf $destination
+fi
+
+# Create folder
+sudo mkdir $destination
+
+echo "📂 Copying build to destination directory..."
+sudo rsync -a build/ $destination
+
+echo "🚀 Deployed frontend! It is now live at http://it2810-11.idi.ntnu.no/project4"
