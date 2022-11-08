c1e98dd53619ccfa465a868d1f8a0a17c892150f
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-11-08 15:36:46 +0100

    ensure background task and dont print out stdout or stderr

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -8,11 +8,6 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
-    ElOption: typeof import('element-plus/es')['ElOption']
-    ElPagination: typeof import('element-plus/es')['ElPagination']
-    ElSelect: typeof import('element-plus/es')['ElSelect']
-    ElTable: typeof import('element-plus/es')['ElTable']
-    ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
