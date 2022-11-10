233909df75b1c6c7681f2a54558119fd38aba3f7
Magnus Rødseth <magnrod@stud.ntnu.no>
2022-11-10 10:42:58 +0100

    Home view

---
+++ frontend/src/components/common/LinkButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Component, PropType } from 'vue'
+
+defineProps({
+  to: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: false,
+    default: 'primary'
+  },
+  icon: {
+    type: Object as PropType<Component>,
+    required: false,
+    default: ''
+  }
+})
+</script>
+<template>
+  <router-link custom :to="to" v-slot="{ navigate }">
+    <el-button @click="navigate" :type="type" :icon="icon"><slot /></el-button>
+  </router-link>
+</template>
