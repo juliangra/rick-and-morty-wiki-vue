6116d44dcb9f418152451cf7a45ea349b1f0d1f1
Victoria Kallerud <victorka@stud.ntnu.no>
2022-11-19 15:29:32 +0100

    fix(#16): fix leaderboard style

--- frontend/src/components/common/LoadingOverlay.vue
+++ frontend/src/components/common/LoadingOverlay.vue
@@ -3,12 +3,15 @@ defineProps<{
   transparent?: boolean
 }>()
 </script>
+
+
+
 <template>
   <div
     class="absolute h-screen w-screen top-0 left-0 opacity-70 z-10 flex justify-center items-center"
     :class="{ 'bg-black': !transparent }"
   >
-    <div
+    <div 
       class="w-12 h-12 border-gray-200 border-4 rounded-full inline-block box-border animate-spin border-b-transparent"
     ></div>
   </div>
